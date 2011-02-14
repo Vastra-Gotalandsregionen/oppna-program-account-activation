c31b09f564ab8cfcce61a6963a9e2455d1cfb436
wmdaros <wmdaros@15d1392f-d3d7-68b2-fc8c-6cff44a5bba1>
2011-02-14 11:23:11 +0000

    - rename packages - OneTimePassword entity and repository

--- trunk/core-bc/modules/portlet/src/main/java/se/vgregion/activation/controllers/controllers/ActivationController.java
+++ trunk/core-bc/modules/portlet/src/main/java/se/vgregion/activation/controllers/controllers/ActivationController.java
@@ -1,11 +1,11 @@
-package se.vgregion.portal.activation.controllers;
+package se.vgregion.activation.controllers.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
-import model.ActivationValidationResponse;
+import se.vgregion.activation.domain.ActivationValidationResponse;
 
 @Controller
 @RequestMapping("VIEW")
