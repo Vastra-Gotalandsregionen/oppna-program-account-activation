d075dcc87ea205c1e9478c805246cd3664aa0985
aasplund@gmail.com <aasplund@gmail.com@15d1392f-d3d7-68b2-fc8c-6cff44a5bba1>
2011-03-04 09:12:42 +0000

    Refactor and clean up

--- trunk/core-bc/composites/webcomp/src/main/java/se/vgregion/activation/controllers/ActivationController.java
+++ trunk/core-bc/composites/webcomp/src/main/java/se/vgregion/activation/controllers/ActivationController.java
@@ -3,115 +3,70 @@ package se.vgregion.activation.controllers;
 import javax.annotation.Resource;
 import javax.portlet.ActionResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.portlet.bind.annotation.ActionMapping;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import se.vgregion.account.services.AccountService;
-import se.vgregion.activation.domain.OneTimePassword;
-import se.vgregion.activation.domain.PublicHash;
 import se.vgregion.activation.formbeans.PasswordFormBean;
-import se.vgregion.activation.formbeans.ValidationFormBean;
-import se.vgregion.activation.validators.FieldMatchValidator;
+import se.vgregion.activation.validators.AccountValidator;
+import se.vgregion.activation.validators.PasswordMatchValidator;
 
 @Controller
 @RequestMapping("VIEW")
 public class ActivationController {
-    private AccountService accountService;
 
-    @Resource(type = FieldMatchValidator.class)
-    private Validator validator;
+    @Resource(type = PasswordMatchValidator.class)
+    private Validator passwordValidator;
 
-    @Autowired
-    public ActivationController(AccountService accountService) {
-        this.accountService = accountService;
-    }
+    @Resource(type = AccountValidator.class)
+    private Validator accountValidator;
 
     @InitBinder("passwordFormBean")
     public void initBinder(WebDataBinder binder) {
-        binder.setValidator(validator);
+        binder.setValidator(passwordValidator);
+        binder.setValidator(accountValidator);
     }
 
     @RenderMapping
-    public ModelAndView showOneTimePasswordForm(ModelMap model,
-            @ModelAttribute("validationFormBean") ValidationFormBean validationFormBean) {
-        System.out.println("Validation");
-
-        model.addAttribute("validationFormBean", new ValidationFormBean());
-        return new ModelAndView("validationForm", model);
+    public String showOneTimePasswordForm() {
+        return "validationForm";
     }
 
     @RenderMapping(params = { "oneTimePassword" })
-    public ModelAndView showPasswordForm(@RequestParam("oneTimePassword") String oneTimePassword, ModelMap model) {
+    public String showPasswordForm(@ModelAttribute PasswordFormBean passwordFormBean, BindingResult result,
+            Model model) {
 
-        PublicHash publicHash = new PublicHash(oneTimePassword);
-        OneTimePassword account = accountService.getAccount(publicHash);
-        String vgrId = account.getVgrId();
-
-        // if (vgrId != oneTimePassword.vgrId) then (were to?)
-        if (!validateOneTimePassword(vgrId, account, model)) {
-            return new ModelAndView("errorForm", model);
+        // Allways valdate onetime password.
+        accountValidator.validate(passwordFormBean, result);
+        if (result.hasErrors()) {
+            return "errorForm";
         }
 
-        PasswordFormBean passwordFormBean = new PasswordFormBean();
-        passwordFormBean.setVgrId(vgrId);
-        passwordFormBean.setOneTimePassword(oneTimePassword);
+        // Workaround to get the errors form validation in actionrequest
+        if (model.asMap().get("errors") != null) {
+            model.addAttribute("org.springframework.validation.BindingResult.passwordFormBean",
+                    model.asMap().get("errors"));
+        }
 
         model.addAttribute("passwordFormBean", passwordFormBean);
-
-        return new ModelAndView("passwordForm", model);
-    }
-
-    private boolean validateOneTimePassword(String vgrId, OneTimePassword account, ModelMap model) {
-        if (account == null) {
-            model.addAttribute("errorMessage", "Activation code does not exist");
-            return false;
-        } else if (!account.isValid()) {
-            model.addAttribute("errorMessage", "Activation code has already been used");
-            return false;
-        } else if (account.hasExpired()) {
-            model.addAttribute("errorMessage", "Activation code has expired");
-            return false;
-        } else if (!vgrId.equals(account.getVgrId())) {
-            model.addAttribute("errorMessage", "Inproper use of Activation code");
-            return false;
-        }
-        return true;
+        return "passwordForm";
     }
 
     @ActionMapping("activate")
     public void activate(@ModelAttribute PasswordFormBean passwordFormBean, BindingResult result,
             ActionResponse response, Model model) {
 
-        validator.validate(passwordFormBean, result);
-
+        passwordValidator.validate(passwordFormBean, result);
         if (result.hasErrors()) {
-            StringBuilder sb = new StringBuilder();
-            for (ObjectError error : result.getGlobalErrors()) {
-                sb.append("global: " + error.getObjectName() + " " + error.getDefaultMessage() + ";");
-            }
-            for (ObjectError error : result.getFieldErrors()) {
-                sb.append("filed: " + error.getObjectName() + " " + error.getDefaultMessage() + ";");
-            }
-            System.out.println("err: " + sb.toString());
-            model.addAttribute("errorMessage", sb.toString());
-            model.addAttribute("org.springframework.validation.BindingResult.passwordFormBean", result);
             model.addAttribute("errors", result);
-
             response.setRenderParameter("oneTimePassword", passwordFormBean.getOneTimePassword());
-            response.setRenderParameter("vgrId", passwordFormBean.getVgrId());
         } else {
             callSetPassword(passwordFormBean.getPassword());
             response.setRenderParameter("success", "true");
@@ -121,11 +76,6 @@ public class ActivationController {
     @RenderMapping(params = { "success" })
     public String success() {
         return "successForm";
-    }
-
-    @RenderMapping(params = { "password" })
-    public String password() {
-        return "passwordForm";
     }
 
     private void callSetPassword(String newPassword) {
--- trunk/core-bc/modules/portlet/src/main/webapp/WEB-INF/jsp/passwordForm.jsp
+++ trunk/core-bc/modules/portlet/src/main/webapp/WEB-INF/jsp/passwordForm.jsp
@@ -12,13 +12,10 @@
         <aui:input type="hidden" name="vgrId" value="${passwordFormBean.vgrId}"/>
         <aui:input type="hidden" name="oneTimePassword" value="${passwordFormBean.oneTimePassword}"/>
         <aui:input type="hidden" name="dominoPassword" value="${passwordFormBean.dominoPassword}"/>
-        <aui:input autocomplete="off" label="new-password" type="password" name="password"
-                   value="${passwordFormBean.password}" helpMessage="Skriv in ett nytt lösenord"/>
+        <aui:input autocomplete="off" label="new-password" type="password" name="password" value="${passwordFormBean.password}" helpMessage="Skriv in ett nytt lösenord"/>
         <form:errors path="passwordFormBean.password" />
-        <aui:input autocomplete="off" label="enter-again" type="password" name="passwordCheck"
-                   value="${passwordFormBean.passwordCheck}" helpMessage="Repetera ditt lösenord"/>
+        <aui:input autocomplete="off" label="enter-again" type="password" name="passwordCheck" value="${passwordFormBean.passwordCheck}" helpMessage="Repetera ditt lösenord"/>
         <form:errors path="passwordFormBean.passwordCheck" />
-        
     </aui:fieldset>
     <aui:button-row>
         <aui:button type="submit" value="save"/>
