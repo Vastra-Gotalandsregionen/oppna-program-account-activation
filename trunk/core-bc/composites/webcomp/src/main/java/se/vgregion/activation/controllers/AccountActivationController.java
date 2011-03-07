e463969e8e1f32b4fab91ce2373741098aa00f27
aasplund@gmail.com <aasplund@gmail.com@15d1392f-d3d7-68b2-fc8c-6cff44a5bba1>
2011-03-07 14:29:03 +0000

    Adding controller functionallity for domino users

---
+++ trunk/core-bc/composites/webcomp/src/main/java/se/vgregion/activation/controllers/AccountActivationController.java
@@ -0,0 +1,109 @@
+package se.vgregion.activation.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.ActionResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import se.vgregion.activation.formbeans.PasswordFormBean;
+
+@Controller
+@RequestMapping(value = "VIEW")
+public class AccountActivationController {
+
+    protected Validator passwordMatchValidator;
+    protected Validator otpLoginValidator;
+    protected Validator dominoLoginValidator;
+
+    @InitBinder("passwordFormBean")
+    public void initBinder(WebDataBinder binder) {
+        binder.setValidator(passwordMatchValidator);
+        binder.setValidator(otpLoginValidator);
+        binder.setValidator(dominoLoginValidator);
+    }
+
+    public AccountActivationController(Validator otpLoginValidator, Validator dominoLoginValidator,
+            Validator passwordMatchValidator) {
+        this.otpLoginValidator = otpLoginValidator;
+        this.passwordMatchValidator = passwordMatchValidator;
+        this.dominoLoginValidator = dominoLoginValidator;
+    }
+
+    @RenderMapping(params = { "oneTimePassword" })
+    public String showPasswordFormForOTP(@ModelAttribute PasswordFormBean passwordFormBean, BindingResult result,
+            Model model) {
+        return vadlidateAndShowForm(otpLoginValidator, passwordFormBean, result, model);
+    }
+
+    @ActionMapping(params = { "oneTimePassword" })
+    public void activateAccountWithOTP(@ModelAttribute PasswordFormBean passwordFormBean, BindingResult result,
+            ActionResponse response, Model model) {
+        Map<String, String[]> renderParams = new HashMap<String, String[]>();
+        renderParams.put("oneTimePassword", new String[] { passwordFormBean.getOneTimePassword() });
+        setNewPassword(passwordFormBean, result, response, model, renderParams);
+    }
+
+    @RenderMapping(params = { "vgrId" })
+    public String showPasswordFormForDominoUser(@ModelAttribute PasswordFormBean passwordFormBean,
+            BindingResult result, Model model) {
+        return vadlidateAndShowForm(dominoLoginValidator, passwordFormBean, result, model);
+    }
+
+    @ActionMapping(params = { "vgrId" })
+    public void activateAccountAsDominoUser(@ModelAttribute PasswordFormBean passwordFormBean,
+            BindingResult result, ActionResponse response, Model model) {
+        Map<String, String[]> renderParams = new HashMap<String, String[]>();
+        renderParams.put("vgrId", new String[] { passwordFormBean.getVgrId() });
+        setNewPassword(passwordFormBean, result, response, model, renderParams);
+    }
+
+    private String vadlidateAndShowForm(Validator validator, @ModelAttribute PasswordFormBean passwordFormBean,
+            BindingResult result, Model model) {
+
+        // Always validate login password.
+        validator.validate(passwordFormBean, result);
+        if (result.hasErrors()) {
+            return "errorForm";
+        }
+
+        // Workaround to get the errors form validation in actionrequest
+        if (model.asMap().get("errors") != null) {
+            model.addAttribute("org.springframework.validation.BindingResult.passwordFormBean",
+                    model.asMap().get("errors"));
+        }
+        return "passwordMatchForm";
+    }
+
+    private void setNewPassword(@ModelAttribute PasswordFormBean passwordFormBean, BindingResult result,
+            ActionResponse response, Model model, Map<String, String[]> renderParamters) {
+        passwordMatchValidator.validate(passwordFormBean, result);
+        if (result.hasErrors()) {
+            model.addAttribute("errors", result);
+            response.setRenderParameters(renderParamters);
+        } else {
+            callSetPassword(passwordFormBean.getPassword());
+            response.setRenderParameter("success", "true");
+        }
+    }
+
+    @RenderMapping(params = { "success" })
+    public String success(Model model) {
+        model.asMap().clear();
+        return "successForm";
+    }
+
+    private void callSetPassword(String newPassword) {
+        System.out.println("pw: " + newPassword);
+    }
+}
---
+++ trunk/core-bc/composites/webcomp/src/main/java/se/vgregion/activation/controllers/LoginController.java
@@ -0,0 +1,27 @@
+package se.vgregion.activation.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+@Controller
+@RequestMapping(value = "VIEW")
+public class LoginController {
+
+    @RenderMapping
+    public String showDefaultPasswordForm(Model model) {
+        return "otpLogin";
+    }
+
+    @RenderMapping(params = { "loginType=otp" })
+    public String showOtpPasswordForm(Model model) {
+        return "otpLogin";
+    }
+
+    @RenderMapping(params = { "loginType=vgrId" })
+    public String showDominoPasswordForm() {
+        return "dominoLogin";
+    }
+
+}
