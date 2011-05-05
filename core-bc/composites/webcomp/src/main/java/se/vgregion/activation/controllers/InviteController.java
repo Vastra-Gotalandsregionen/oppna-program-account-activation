3262c60a913e41bbc0098b16e327713d61bfcdb5
patrik.bergstrom <patrik.bergstrom@softronic.se>
2011-05-05 06:40:38 +0000

    create account and refactoring

--- core-bc/composites/webcomp/src/main/java/se/vgregion/activation/controllers/InviteController.java
+++ core-bc/composites/webcomp/src/main/java/se/vgregion/activation/controllers/InviteController.java
@@ -2,21 +2,42 @@ package se.vgregion.activation.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
 import org.springframework.web.portlet.bind.annotation.ActionMapping;
 import se.vgregion.activation.formbeans.ExternalUserFormBean;
+
+import javax.portlet.PortletRequest;
+import java.util.Map;
 
 @Controller
 @RequestMapping("VIEW")
 public class InviteController {
 
     @RequestMapping
-    public String showExternalUserInvite(@ModelAttribute ExternalUserFormBean externalUserFormBean, Model model) {
-        if (externalUserFormBean == null) {
-            model.addAttribute("externalUserFormBean", new ExternalUserFormBean());
+    public String showExternalUserInvite(@ModelAttribute ExternalUserFormBean externalUserFormBean, Model model,
+                                         PortletRequest req) {
+        if (externalUserFormBean.getSponsorVgrId() == null) {
+            //Meaning first request
+            Map<String, String> userInfo = (Map<String, String>) req.getAttribute(PortletRequest.USER_INFO);
+            String userId;
+            if (userInfo != null) {
+                userId = userInfo.get(PortletRequest.P3PUserInfos.USER_LOGIN_ID.toString());
+            } else {
+                throw new IllegalStateException("Du måste vara inloggad.");
+            }
+            externalUserFormBean.setSponsorVgrId(userId);
         }
         return "externalUserInvite";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(Exception ex) {
+        ModelAndView modelAndView = new ModelAndView("externalUserError");
+        modelAndView.addObject("errorMessage", ex.getMessage());
+        return modelAndView;
     }
 
     @ActionMapping
@@ -26,6 +47,8 @@ public class InviteController {
         // 2: no outstanding invites
         // 3: user already in PK
         // 4: mandatory information provided
+
+        System.out.println(externalUserFormBean.toString());
 
 
     }
