db938fcab3de9c1f65ac6f45007e8281c6e9775e
patrik.bergstrom <patrik.bergstrom@softronic.se>
2011-05-18 09:10:49 +0000

    - store ExternalUserStructure - javascript execution order matters

--- core-bc/composites/svc/src/main/java/se/vgregion/account/services/util/StructureQueryUtil.java
+++ core-bc/composites/svc/src/main/java/se/vgregion/account/services/util/StructureQueryUtil.java
@@ -9,22 +9,26 @@ import se.vgregion.create.domain.ExternalUserStructure;
  */
 public class StructureQueryUtil {
 
-    public String whereClause(String[] queryParts) {
+    public String whereClause(String[] queryParts, boolean like) {
         if (queryParts == null || queryParts.length == 0) return "";
 
         StringBuilder whereClause = new StringBuilder("where ");
         int length = queryParts.length;
-        for (int i = 0; i < length-1 ; i++) {
+        for (int i = 0; i < length - 1; i++) {
             String queryPart = queryParts[i];
             StringBuilder joinClause = new StringBuilder("join s");
-            for (int j = length-1; j > i; j--) {
+            for (int j = length - 1; j > i; j--) {
                 joinClause.append(".parent");
             }
-            joinClause.append(" s"+(length-i)+" ");
+            joinClause.append(" s" + (length - i) + " ");
             whereClause.insert(0, joinClause);
-            whereClause.append("lower(s"+(length-i)+".name) = ?"+(i+1)+" and ");
+            whereClause.append("lower(s" + (length - i) + ".name) = ?" + (i + 1) + " and ");
         }
-        whereClause.append("lower(s.name) like ?"+ length);
+        if (like) {
+            whereClause.append("lower(s.name) like ?" + length);
+        } else {
+            whereClause.append("lower(s.name) = ?" + length);
+        }
 
         return whereClause.toString();
     }
